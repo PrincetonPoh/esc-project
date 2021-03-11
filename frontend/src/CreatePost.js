0cb1d19aea3163bfd53ea0f10193deb4c1acbd76
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2021-03-11 21:25:36 +0800

    Added create post page

---
+++ frontend/src/CreatePost.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CreatePost.css';
+import CreatePostForm from './CreatePostForm';
+
+function CreatePost(){
+
+    return (    
+        <div>
+            <h1> Create a Post </h1>
+            <CreatePostForm/>
+        </div>
+    );
+}
+export default CreatePost;
