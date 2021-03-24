2b9565268a6c529b99af315250e55b0610cedec3
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2021-03-24 23:41:33 +0800

    Improve SigninPopup form

--- frontend/src/components/SigninPopup.js
+++ frontend/src/components/SigninPopup.js
@@ -3,67 +3,92 @@ import '../styles/Popup.css';
 import cross_icon from '../media/cross_icon.png';
 
 class SigninPopup extends Component {
-  warning = {
-    creds: false,
-    password: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = { 
+      creds: '',
+      password: '',
+      credsWarning: null,
+      passwordWarning: null,
+      formWarning: null
+    };
+  }
 
   handleClick = () => {
     this.props.toggle(); 
   };
 
-  handleSignin = () => {
-    var signin_creds = document.forms["signin-form"]["signin-creds"].value;
-    var signin_password = document.forms["signin-form"]["signin-password"].value;
-    var valid_entries = true; 
-    if (signin_creds=="") {
-      this.warning.creds = true;
-      alert("Please fill in the required fields to sign in.");
-      valid_entries = false; 
+  handleChange = (event) => {
+    const target = event.target; 
+    const value = target.value; 
+    const name = target.name; 
+    this.setState({
+        [name]: value
+    });
+    console.log("creds: "+this.state.creds+"\npassword: "+this.state.password+"\ncredsWarning: "+this.state.credsWarning);
+    if (value == '') {
+      this.setState({
+        [name+"Warning"]: <p class="inputWarning"> This field is required! </p>
+      });
+    } else {
+      this.setState({
+        [name+"Warning"]: null
+      });
     }
-    else if (signin_password=="") {
-      this.warning.password = true;
-      alert("Please fill in the required fields to sign in.");
-      valid_entries = false; 
+  }
+
+  checkValidity = (name) => { // can check for other illegal input here too
+    if (this.state[name]=='') {
+      this.setState({
+        [name+"Warning"]: <p class="inputWarning"> This field is required! </p>
+      });
+      return false;
+    } else {
+      return true;
     }
-    if (valid_entries) {
+  }
+
+  handleSignin = (e) => {
+    var credsValid = this.checkValidity("creds");
+    var passwordValid = this.checkValidity("password");
+    if (credsValid && passwordValid) {
       var signin_auth_success = true; // add auth here maybe 
       if (signin_auth_success) {
         this.props.signin();
       } else { 
         alert("auth failed!"); 
       }   
-    };
+    } else {
+      this.setState({
+        formWarning: <p class="formWarning"> Please fill in the required fields to sign in! </p>
+      });
+    }
+    e.preventDefault();
   };
 
-  // handleSubmit(event) {
-  //   alert("submited: "+this.state.value);
-  //   event.preventDefault();
-  // }
-
   render() {
+    
     return ( 
       <div>
         <div id="signinpopup-background" onClick={this.handleClick}> </div>
         <div id="signin-popup" class="dropshadow">
           <img id="popup-cross-icon" src={cross_icon} onClick={this.handleClick} class="navbar-icons dropshadow"/>
-          <p> Sign in to Scratchbac </p>
-          <form id="signin-form" name="signin-form"> 
-            <p class="popup-contents"> Username / Email / Mobile Number </p>
-            <input type="text" id="signin-creds" name="creds" placeholder="Username" class="text-fields dropshadow"/>
-            <div id="signin-creds-warning" class="warning"> {this.warning.creds ? <p> no empty pls </p> : null}  </div>
-            <p class="popup-contents"> Password <a href="#" id="forgot-password"> Forgot Password? </a></p>
-            <input type="text" id="signin-password" name="password" placeholder="Password" class="text-fields dropshadow"/>
-            {/* <input type="submit" value="Sign in" /> */}
+          <h2> Sign in to Scratchbac </h2>
+
+          <form id="signin-form" name="signin-form" onSubmit={this.handleSignin}> 
+            <label class="popup-contents"> Username / Email / Mobile Number </label>
+            <input type="text" id="signin-creds" name="creds" onChange={this.handleChange} class="text-fields dropshadow"/>
+            {this.state.credsWarning}
+            
+            <label class="popup-contents"> Password </label>
+            <a href="#" id="forgot-password"> Forgot Password? </a> 
+            <input type="text" id="signin-password" name="password" onChange={this.handleChange} class="text-fields dropshadow"/>
+            {this.state.passwordWarning}
+
+            {this.state.formWarning}
+            <input id="signin-popup-button" type="submit" value="Sign in"  class="dropshadow"/>
           </form>
-          {/* <form onSubmit={this.handleSubmit}> 
-            <label> 
-              Username / Email / Mobile Number
-              <input type="text" value={this.state.value}/> 
-            </label>
-            <input type="submit" value="Sign in" />
-          </form> */}
-          <button id="signin-popup-button" onClick={this.handleSignin} class="navbar-buttons dropshadow"> Sign in </button>
+ 
         </div>
       </div>
     )
