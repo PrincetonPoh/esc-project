753a4eb5ae27b43899236de3e1c4c1e26c488f93
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2021-03-25 12:39:22 +0800

    Modify Signup form to check valid fields

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -7,27 +7,102 @@ import axios from 'axios';
 import '../styles/Signup.css';
 
 class Signup extends Component {
-
-    handleSubmit = e => {
-        e.preventDefault(); //Disable refresh/relaod
-        const data = {
-            first_name: this.firstName,
-            last_name: this.lastName,
-            username: this.username,
-            hp: this.hp,
-            email: this.email,
-            password: this.password,
-            confirm_password: this.confirmPassword
+    constructor(props) {
+        super(props);
+        this.state = { 
+            firstName: '',
+            lastName: '',
+            username: '',
+            hp: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            
+            firstNameWarning: null,
+            lastNameWarning: null,
+            usernameWarning: null,
+            hpWarning: null,
+            emailWarning: null,
+            passwordWarning: null,
+            confirmPasswordWarning: null,
+            formWarning: null
         };
-        console.log(data);
-        axios.post("http://localhost:1337/users/createUser", {
-            "user_id": Math.floor(Math.random() * 1000),
-            "phoneNumber": data.hp,
-            "userName": data.username,
-            "emailAddress": data.email,
-            "password": data.password
-        }).then((response) => alert("Sign-up successful.")).catch(error => alert("Error."))
-        this.props.history.push('/');//Force push
+    }
+
+    handleChange = (event) => {
+        const target = event.target; 
+        const value = target.value; 
+        const name = target.name; 
+        this.setState({
+            [name]: value
+        });
+        // this[name] = value;
+        console.log("firstName: "+this.state.firstName+"\nlastName: "+this.state.lastName+"\nfirstNameWarning: "+this.state.firstNameWarning+"\nlastNameWarning: "+this.state.lastNameWarning);
+        if (value == '') {
+          this.setState({
+            [name+"Warning"]: <p class="inputWarning"> This field is required! </p>
+          });
+        } else {
+          this.setState({
+            [name+"Warning"]: null
+          });
+        }
+      }
+
+    checkValidity = (name) => { // can check for other illegal input here too
+        if (this.state[name]=='') {
+            this.setState({
+                [name+"Warning"]: <p class="inputWarning"> This field is required! </p>
+            });
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault(); //Disable refresh/relaod 
+        var firstNameValid = this.checkValidity("firstName");
+        var lastNameValid = this.checkValidity("lastName");
+        var usernameValid = this.checkValidity("username");
+        var hpValid = this.checkValidity("hp");
+        var emailValid = this.checkValidity("email");
+        var passwordValid = this.checkValidity("password");
+        var confirmPasswordValid = this.checkValidity("confirmPassword");
+        if (firstNameValid && lastNameValid && usernameValid && hpValid && emailValid && passwordValid && confirmPasswordValid) {
+            if (this.state.password != this.state.confirmPassword) { // all fields filled, check if passwords match
+                this.setState({
+                    confirmPasswordWarning: <p class="inputWarning"> The passwords you entered do not match! </p>
+                });
+            } else { // all fields filled, passwords match
+                this.setState({ 
+                    confirmPasswordWarning: null
+                });
+                const data = {
+                    first_name: this.state.firstName,
+                    last_name: this.state.lastName,
+                    username: this.state.username,
+                    hp: this.state.hp,
+                    email: this.state.email,
+                    password: this.state.password,
+                    // confirm_password: this.state.confirmPassword // already checked if passwords match before triggering this
+                };
+                console.log(data);
+                axios.post("http://localhost:1337/users/createUser", {
+                    "user_id": Math.floor(Math.random() * 1000),
+                    "phoneNumber": data.hp,
+                    "userName": data.username,
+                    "emailAddress": data.email,
+                    "password": data.password
+                }).then((response) => alert("Sign-up successful.")).catch(error => alert("Error."))
+                // this.props.history.push('/');//Force push
+            }
+        } else { // at least one field empty 
+            this.setState({
+                formWarning: <p class="formWarning form-item"> Please fill in the required fields to sign in! </p>
+              });
+        }
+        
     }
 
     render() {
@@ -36,47 +111,56 @@ class Signup extends Component {
                 <h1>Sign up</h1>
                 <form id="signup-form" onSubmit={this.handleSubmit}>
                     <div id="formPlaintextEmail" class="form-item">
-                        <label >
+                        <label>
                             First Name
                         </label>
-                        <input type="firstName" class="signup-form-input" onChange={e => this.firstName = e.target.value} />
+                        <input type="firstName" name="firstName" class="signup-form-input" onChange={this.handleChange} />
+                        {this.state.firstNameWarning}
                     </div>
                     <div id="formGroupLastName" class="form-item">
-                        <label >
+                        <label>
                             Last Name
                         </label>
-                        <input type="lastName" class="signup-form-input" onChange={e => this.lastName = e.target.value} />
+                        <input type="lastName" name="lastName" class="signup-form-input" onChange={this.handleChange} />
+                        {this.state.lastNameWarning}
                     </div>
                     <div id="formGroupUserName" class="form-item">
                         <label>
                             Username
                         </label>
-                        <input class="signup-form-input" onChange={e => this.username = e.target.value}/>
+                        <input type="username" name="username" class="signup-form-input" onChange={this.handleChange}/>
+                        {this.state.usernameWarning}
                     </div>
                     <div id="formGroupHP" class="form-item">
                         <label>
                             Mobile Number
                         </label>
-                        <input type="tel" class="signup-form-input" onChange={e => this.hp = e.target.value}/>
+                        <input type="tel" name="hp" class="signup-form-input" onChange={this.handleChange}/>
+                        {this.state.hpWarning}
                     </div>
                     <div id="formGroupEmail" class="form-item">
-                        <label >
+                        <label>
                             Email
                         </label>
-                        <input type="email" class="signup-form-input" onChange={e => this.email = e.target.value} />
+                        <input type="email" name="email" class="signup-form-input" onChange={this.handleChange}/>
+                        {this.state.emailWarning}
                     </div>
                     <div id="formGroupPassword" class="form-item">
-                        <label >
+                        <label>
                             Password
                         </label>
-                        <input type="password" class="signup-form-input" onChange={e => this.password = e.target.value} />
+                        <input type="password" name="password" class="signup-form-input" onChange={this.handleChange}/>
+                        {this.state.passwordWarning}
                     </div>
                     <div id="formGroupConfirmPassword" class="form-item">
-                        <label >
+                        <label>
                             Confirm Password
                         </label>
-                        <input type="password" class="signup-form-input" onChange={e => this.confirmPassword = e.target.value} />
+                        <input type="password" name="confirmPassword" class="signup-form-input" onChange={this.handleChange}/>
+                        {this.state.confirmPasswordWarning}
                     </div>
+                    
+                    {this.state.formWarning}
                     <input id="signup-form-button" type="submit" value="Create Account"/>
                 </form>
             </div>
