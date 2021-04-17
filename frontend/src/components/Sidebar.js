b9ec25bd7c54f3501ba1d4d2085c1ea537e44097
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2021-04-17 15:32:21 +0800

    Update sidebar with working links

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -12,15 +12,13 @@ class Sidebar extends Component {
         <div id="sidebarpopup-background" onClick={this.handleClick}> </div>
         <div id="sidebar"> 
           <div id="sidebar-content" class="dropshadow"> 
-            <a href="#" class="sidebar-items"> Home  </a> 
+            <a href="https://scratchbac.com/" class="sidebar-items"> Scratchbac </a> 
             <hr/> 
-            <a href="#" class="sidebar-items"> Browse Posts </a>
+            <a href="/" class="sidebar-items"> Browse Posts </a>
             <hr/> 
-            <a href="#" class="sidebar-items"> Latest News </a> 
+            <a href="https://scratchbac.com/our-blog/" class="sidebar-items"> Latest News </a> 
             <hr/> 
-            <a href="#" class="sidebar-items"> About Scratchbac </a>
-            <hr/>
-            <a href="#" class="sidebar-items"> Sponsors </a>
+            <a href="https://scratchbac.com/about-us/" class="sidebar-items"> About Scratchbac </a>
           </div>
         </div>
       </div>
