9584d59f02f132941f2e0d47d35cb9bb56b21ec1
Jukocross <jukocross@gmail.com>
2021-04-22 00:25:27 +0800

    Revert to dateOfPostEvent

--- README.md
+++ README.md
@@ -1,6 +1,57 @@
-# esc-project
+# Backend node module
+Welcome to the **BACKEND** :) 
+# Table of contents
+* [Tech stack](#tech-stack)
+* [Getting Started](#getting-started)
+* [What is the file structure? 🗂](#What-is-the-file-structure?-🗂)
+* [Tests](#Tests)
+* [Further possible improvements](#Further-possible-improvements)
+* [Backend Contributions](#Backend-Contributions)
+# General info / Tech stack
+The language used is 100% javascript. Sqlite3 is used as our database as it offers a lot of flexibility in just a singular file. We use Knex as our query builder to write the "sql" code. We also enlisted the help of DBbrowser ORM tool that helps us visualise and quickly build the database.
+# Getting started
+How to run our backend api? 🏃
+1. go to the backendPrinceton branch
+2. go to the file 'blueprint' and insert the latest postman json file into postman
+3. type `npm install` to install all the node dependencies
+4. type `npm start`
+    - You should see the test "Server is running on port 1337"
+5. test the apis on postman :)
 
-`main` branch now has all the frontend code. To run:
+# What is the file structure? 🗂
+This is our first attempt at trying to modularize our codes by having APIs with similar functions be separated from each other using routes. We also tried to write repeatable code by using our own middleware.
+- route_paths
+    - Purpose is to route the different files and directory into the main server.js and allow better separation of function for each file.
+- routes
+    - Contains the api calls for _users_, _posts_, _comments_, _location_ and _authentication_.
+- middleware
+    - Contains reusable code across the different files in "routes" directory. (e.g uuid generator and jwt auth checker)
+- db
+    - Contains the querys to the database. This is separated from the API calls to allow for more complex database calls in the future.
 
-1. Type `npm install`
-2. Type `npm start`
+
+# Tests
+We use supertest and jest frameworks in our unit testing. We mainly do black box testing to ensure that the api returns what we expect them to. A separate testing database called test.sqlite3 is used.
+
+How to run the test?
+1. type the code `npm run test:watch`
+
+# Further possible improvements
+1. Create a database schema to check the input types, ensure consistent data types
+2. Create unit test for jwt authentication
+# Backend Contributions
+Most of the work are done primarily by Princeton and Wanghan (backend team). Due to overlapping workscope, the below allocations indicate who is responsible for that section but not necessarily who worked on it.
+## Princeton Poh
+- Nodejs architecture design
+- Setup git workflow(git lens, github desktop)
+- Setup postman, knex(query builder), DBbrowser(ORM)
+- Email verification server
+- JWT authentication
+- Unit test for `comments.test.js`
+- Unit test for authentication
+## Wang Han
+- SQLite3 database design
+- Fuzzy search implementation
+- Setup test database.
+- Unit test for `users.test.js`
+- Unit test for `posts.test.js`
---
+++ routes/auth.js
@@ -0,0 +1,185 @@
+require('dotenv').config()
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const db = require('../db/escData');
+const { ref } = require('../db/knex');
+const uuid = require('../middleware/uuid');
+const nodemailer = require("nodemailer");
+const router = express.Router();
+
+const ipAddress = "localhost:1337"
+
+// for testing. 
+let refreshTokens = []
+let uuidList = []
+
+// const posts = [
+//     {
+//       user_id: 'Kyle',
+//       title: 'Post 1'
+//     },
+//     {
+//       username: 'Jim',
+//       title: 'Post 2'
+//     }
+// ]
+
+/////////////////////////////////////////////////////
+
+router.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.query.token)
+    res.status(204)
+})
+
+// insert login credentials
+router.post('/createUser', async (req, res) => {
+    // Authenticate User
+    const user_id = uuid.generateUuid();
+    req.body.user_id = user_id;
+    uuidList.push(user_id)
+
+    try {
+        const result = await db.createUser(req.body);
+        const accessToken = generateAccessToken(req.body)
+        const refreshToken = jwt.sign(req.body, process.env.REFRESH_TOKEN_SECRET)
+        refreshTokens.push(refreshToken)
+        
+        res.json({ success_user_id: user_id, accessToken: accessToken, refreshToken: refreshToken })
+    } catch {
+        res.status(409).json({message: "invalid data fills. probably non-unique phone number/username"})
+    }
+    
+})
+
+
+router.get('/login', async (req, res) => {
+    const userName = req.query.userName
+    const password = req.query.password
+    const userData = await db.getUserByUserName(userName)
+    try{
+        if (userData[0].password != password) return res.status(409).json({message:"Incorrect password"})
+    } catch(error){
+        console.log(error);
+        return res.status(409).json({message:'User not available'});
+    }
+
+    const accessToken = generateAccessToken(req.body)
+    const refreshToken = jwt.sign(req.body, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    console.log(refreshTokens)
+    
+    res.json({ userName: userName, accessToken: accessToken, refreshToken: refreshToken })
+})
+
+router.post('/token', async (req, res) => {
+    const refreshToken = req.query.token
+    if (refreshToken == null) return res.status(401)
+    if (!refreshTokens.includes(refreshToken)) return res.status(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.status(403)
+      const accessToken = generateAccessToken({ name: user.name })
+      res.json({ accessToken: accessToken })
+    })
+})
+
+
+function generateAccessToken(user){
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn : '35m'})
+}
+
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'randomran9898@gmail.com',
+        pass: 'Randomran98',
+    }
+});
+
+router.get('/verifyEmail', async (req, res) => {
+    const user = {userName: req.query.userName}
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '35m'},
+        (err, emailToken) => {
+            console.log('finished signing jwt and going to send email')
+            const url = `http://${ipAddress}/auth/uploadEmailToken/:${emailToken}`;
+            
+            let mailOptions = {
+                from: "randomran9898@gmail.com",
+                to: req.query.email,
+                subject: 'Confirm Email',
+                text: `Please click this email to confirm your email: ${url}`,
+            }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+            });
+            // res.status(200).json({message: "verification email sent"})
+            res.status(200).json({message: emailToken})
+    },
+    );    
+})
+
+router.get('/uploadEmailToken/:token', async (req, res) => {
+
+    jwt.verify(req.params.token.substr(1), process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+        // if (err) return res.status(403).json({message: "email verification failed ><"})
+        if (err) {
+            console.log("me error is here! :\n\n")
+            console.log(err)
+            return res.status(403).json({message: "email verification failed ><"})
+        }
+        
+        const userDetails = {userName: user.userName,
+                        verificationStatus: 'true'}
+        // update db to say this user is email verified
+        await db.verifyTheUser(userDetails)
+        res.status(200).json({message: "email verification success! Please proceed back to the website"});
+    })
+});
+
+router.get('/checkVerifiedUser', async (req, res) => {
+    try{
+        const verificationStatus = await db.checkVerifiedUser(req.query.userName)
+        res.status(200).json({message:verificationStatus})
+    } catch(error){
+        res.status(409).json({message:"problem retrieving from db"})
+    }
+})
+
+
+// router.use(authenticateToken)
+// /////////////////////////////////////////////////////
+
+
+// // middleware function
+// function authenticateToken(req, res, next) {
+//     const authHeader = req.headers['authorization']
+//     const token = authHeader && authHeader.split(' ')[1]
+//     if (token == null) return res.sendStatus(401)
+  
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//         if (err) return res.sendStatus(403)
+
+//         // if (posts.map(a=>a.username).includes(user.name)){
+//         if (uuidList.includes(user.user_id)){
+//             req.user = user
+//             next()
+//         }
+//         return res.sendStatus(405)
+//     })
+// }
+
+// // dummy posts api call for testing
+// router.get('/posts', authenticateToken, async (req, res) => {
+//     res.json({successful : jwt})
+// })
+
+// module.exports = {authenticateToken};
+
+
+
+module.exports = router;
